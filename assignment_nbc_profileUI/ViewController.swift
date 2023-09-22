ad91b79617f01f507a3cb08bbc5d8193383f7441
Daisy Kim <62759476+doyeonyyy@users.noreply.github.com>
2023-09-22 09:38:18 +0900

    first commit: upload
    upload files

---
+++ assignment_nbc_profileUI/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  assignment_nbc_profileUI
+//
+//  Created by t2023-m0047 on 2023/09/21.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+
+//    let firstView: UIView = {
+//        let secondView = UIView()
+//        secondView.backgroundColor = UIColor.gray
+//        secondView.layer.cornerRadius = 10
+//        secondView.layer.masksToBounds = true
+//        return secondView
+//    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        setupUI()
+    }
+
+//    func setupUI() {
+//
+//        view.addSubview(firstView)
+//
+//        firstView.translatesAutoresizingMaskIntoConstraints = false
+//
+//        firstView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
+//        firstView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
+//        firstView.topAnchor.constraint(equalTo: view.topAnchor, constant: 200).isActive = true
+//        firstView.heightAnchor.constraint(equalToConstant: 40).isActive = true
+//
+//        firstView.layer.cornerRadius = 10
+//        firstView.layer.masksToBounds = true
+//
+//        firstView.backgroundColor = UIColor.gray
+//
+//
+//    }
+}
+
